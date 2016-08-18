1c96def4c310f8cc9cdb99fc49e5bbcaa8256249
Priyam Sarma <priyam.sarma@gmail.com>
2016-08-18 18:07:33 -0400

    Adds delta updates to repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'tweets#index'
+
   devise_for :users
+
+  resources :tweets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
